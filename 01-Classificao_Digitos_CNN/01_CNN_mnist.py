4c5d85fbec083a7e820cdc50e1af4eac911d676d
Lucas Nobre <lucasnobrepro2015@gmail.com>
2020-07-10 08:32:24 -0300

    Change name folder and update files.

--- 01-Classificao_Digitos_CNN/01_CNN_mnist.py
+++ 01-Classificao_Digitos_CNN/01_CNN_mnist.py
@@ -14,7 +14,7 @@ from keras.layers import Dense, Flatten, Dropout
 from keras.utils import np_utils
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
-
+import numpy as np
 
 # separação de dados em treinoe teste.
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -42,11 +42,6 @@ X_test  /= 255
 # fazemos o dummy das classes
 y_train = np_utils.to_categorical(y_train,10)
 y_test = np_utils.to_categorical(y_test ,10)
-
-from numpy.random import seed
-seed(1)
-import tensorflow as tf
-tf.random.set_seed(2)
 
 
 # Criando modelo de classificador.
@@ -92,12 +87,28 @@ model.compile(loss = 'categorical_crossentropy',
 
 # Treinamento
 model.fit(X_train, y_train,
-          batch_size = 128, epochs = 1,
+          batch_size = 128, epochs = 10,
           validation_split=0.3)
 
 
 model.evaluate(X_test,y_test)   
 
+
+# TESTANDO COM UMA UNICA IMAGEM
+# lendo uma imagem
+image_test = X_test[0]
+
+image_test = np.expand_dims(image_test,axis=0)
+
+pred = model.predict(image_test)
+
+y_test
+
+count = 0
+for i in pred[0]:
+    if i > 0.3:
+        print(count)
+    count += 1 
 
 
 
