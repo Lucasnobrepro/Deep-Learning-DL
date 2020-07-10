e377fbd7cbee024f3b52be5272e55fc06c5ab6d1
Lucas Nobre <lucasnobrepro2015@gmail.com>
2020-07-10 08:32:24 -0300

    Change name folder and update files.

---
+++ 03-Dog-cat/dog-cat.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 18:34:48 2020
+
+@author: Lucas Nobre
+"""
+from keras.models import Sequential
+from keras.layers import MaxPooling2D, Flatten, Conv2D, Dense, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import BatchNormalization
+
+# Criando modelo
+model = Sequential()
+
+# Primeira camada de convolução
+model.add(Conv2D(32,
+                 (3,3),
+                 input_shape=(64,64,3),
+                 activation='relu'))
+
+# acelerando o processamento atrvés da normalização dos dados.
+model.add(BatchNormalization())
+
+# Camada de pooling.
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+# Segunda camada de convolução
+model.add(Conv2D(32,
+                 (3,3),
+                 input_shape=(64,64,3),
+                 activation='relu'))
+
+# acelerando o processamento atrvés da normalização dos dados.
+model.add(BatchNormalization())
+
+# Camada de pooling.
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+# Camada de flatten
+model.add(Flatten())
+
+#Criando uma rede neural densa.
+model.add(Dense(units = 128,
+                activation='relu'))
+# zerando 20% das entradas para evitar overfitting
+model.add(Dropout(0.2))
+
+#Criando uma rede neural densa.
+model.add(Dense(units = 128,
+                activation='relu'))
+# zerando 20% das entradas para evitar overfitting
+model.add(Dropout(0.2))
+# saida
+model.add(Dense(units = 1,
+                activation='sigmoid'))
+
+model.compile(optimizer='Adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+    
+# argumentetion e normalizacao
+train_generator = ImageDataGenerator(rescale=1./255,         # Normalizacao
+                                     rotation_range=0.7,     # grau de rotacao
+                                     horizontal_flip=True,   # giro horizontal
+                                     shear_range=0.2,        # direcao dos pixels
+                                     height_shift_range=0.07,# faixa de mudanca da altura
+                                     zoom_range=0.2 )        # zoom
+# normalizacao
+test_generator = ImageDataGenerator(rescale=1./255)
+
+
+path_train = '/home/lucas/Documentos/Git-Repositorios/Deep-Learning-DL/03-Dog-cat/dataset/training_set'
+base_train = train_generator.flow_from_directory(path_train,    
+                                                 target_size=(64,64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
+
+path_test = '/home/lucas/Documentos/Git-Repositorios/Deep-Learning-DL/03-Dog-cat/dataset/test_set'
+base_test = test_generator.flow_from_directory(path_test,    
+                                                 target_size=(64,64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
+
+
+model.fit_generator(base_train, steps_per_epoch=4000/32,
+                    epochs=5, validation_data=base_test,
+                    validation_steps=1000/32)
+
+
+
+
+
+
